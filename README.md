3990450a1a6fbad3184e1304f728882ffaabffb4
冯承运 <790322000@qq.com>
2023-12-07 14:40:37 +0800

    页面跳转的loadingPage页面添加了进度条

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-## useNewContract
-    是new出合约的hooks目前被关闭了，需要把 ``&& false`` 删除
+
 ## 翻译
     const { t, i18n } = useTranslation();
-
     翻译 t('xxx')
     切换 i18n.changeLanguage(key)   type[cn,en]
 ## useChangeChain
--- src/router/LoadingPage/index.tsx
+++ src/router/LoadingPage/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useEffect } from 'react'
+import Nprogress from 'nprogress'
+import './index.scss'
 
 const LoadingPage = () => {
+  useEffect(() => {
+    Nprogress.start()
+
+    return () => {
+      Nprogress.done(true)
+    }
+  }, [])
+
+
   return (
-    <div>LoadingPage...</div>
+    <div className="loadingPage">
+
+    </div>
   )
 }
 
